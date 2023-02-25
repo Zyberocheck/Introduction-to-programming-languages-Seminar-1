90fc74974518161f18a5a5fb37fe4a38f74bdef4
Alexandr <sasha-bragin@mail.ru>
2023-02-26 00:31:16 +0300

    Task3 Задача3 1:54

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,58 @@
 // 5 -> Пятница
 
 
+
+Console.WriteLine("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+    Console.WriteLine("Mon");
+    break;
+    case 2:
+    Console.WriteLine("Tues");
+    break;
+}
+int Mon = 1;
+// int Tues = 2;
+// int Wedn = 3;
+// int Thurs = 4;
+// int Fri = 5;
+// int Satur = 6;
+// int Sun = 7;
+
+// int dayweek = Console.ReadLine();
+if (1)
+{
+    Console.WriteLine("Mon");
+}
+// if (dayweek == 2)
+// {
+//     Console.WriteLine("Tues");
+// }
+// if (dayweek == 3)
+// {
+//     Console.WriteLine("Wedn");
+// }
+// if (dayweek == 4)
+// {
+//     Console.WriteLine("Thurs");
+// }
+// if (dayweek == 5)
+// {
+//     Console.WriteLine("Fri");
+// }
+// if (dayweek == 6)
+// {
+//     Console.WriteLine("Satur");
+// }
+// if (dayweek == 7)
+// {
+//     Console.WriteLine("Sun");
+// }
+
+
+// Console.WriteLine("Введите ваше имя ");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
